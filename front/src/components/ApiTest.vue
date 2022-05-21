931e22ba97e818ccbc91f23a9526220fe16d48cc
mochi-yu <mochi.2225.it@gmail.com>
2022-05-21 16:50:29 +0900

    add: apiに関する処理を記述

---
+++ front/src/components/ApiTest.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>API Test</p>
+    <p>API response from backend: {{ res }}</p>
+    <button @click="testAPI">New random number</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+let baseUrl = "https://idea-mixer-hackathon2205.herokuapp.com/"
+
+export default {
+  data () {
+    return {
+      res: null
+    }
+  },
+  methods: {
+    testAPI () {
+      const path = baseUrl + '/api/random'
+      axios.get(path)
+        .then( response => (this.res = response) )
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  created () {
+    this.testAPI()
+  }
+}
+</script>
