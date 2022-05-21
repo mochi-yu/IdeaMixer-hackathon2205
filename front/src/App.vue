5cc96f91c66dbe844778aa63fadeec2438707c96
mochi-yu <mochi.2225.it@gmail.com>
2022-05-21 19:28:57 +0900

    add: DBに関するフロントエンドを追加

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,6 +3,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
+      <router-link to="/DBtest">DB</router-link> |
       <router-link to="/test">Test</router-link>
     </nav>
     <router-view/>
